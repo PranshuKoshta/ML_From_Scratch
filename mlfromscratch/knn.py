d990f51a267d92628b5c433282a2b873200bd3b7
Pranshu Koshta <pranshukoshta7@gmail.com>
2025-07-13 00:48:30 +0530

    knn done

--- mlfromscratch/knn.py
+++ mlfromscratch/knn.py
@@ -1,26 +1,55 @@
+import numpy as np
+from collections import Counter
 
+def euclean_distance(x1,x2):
+    return np.sqrt(np.sum((x1 - x2)**2))
 
 class KNN:
-    def __init__(self, k=3):
+    def __init__(self, k=5):
         self.k = k
 
     def fit(self,X,y):
-        pass
+        self.X_train = X
+        self.y_train = y
     
     def predict(self, X):
-        pass
+        y_pred = [ self.predictor(x) for x in X ]
+        return np.array(y_pred)
+
+    def predictor(self,x):
+        # compute distance
+        distances = [ euclean_distance(x,x_train) for x_train in self.X_train ]
+
+        # find k nearest neighbors
+        k_nearest = np.argsort(distances)[:self.k]
+
+        k_nearest_labels = [ self.y_train[i] for i in k_nearest]
+
+        # majority vote, most common class label
+        most_common = Counter(k_nearest_labels).most_common(1)[0][0]
+        return most_common
+
 
 
 
 
 if __name__ == "__main__" :
-    from matplotlib.colors import ListedColormap
     from sklearn import datasets
     from sklearn.model_selection import train_test_split
     import pandas as pd
 
-    iris = datasets.load_iris(as_frame=True)
-    X,y = iris.data, iris.target
+    digits = datasets.load_digits()
+    X,y = digits.data, digits.target
 
-    X_train,y_train,X_test,y_test = train_test_split(X,y, test_size= 0.2, random_state=123 )
-    
+    X_train,X_test,y_train,y_test = train_test_split(X,y, test_size= 0.2, random_state=123)
+
+    clf = KNN()
+    clf.fit(X_train,y_train)
+    y_pred = clf.predict(X_test)
+    print(y_pred)
+    print(y_test)
+
+    accuracy = np.sum(y_pred == y_test) / len(y_test)
+    print(accuracy)
+
+
---
+++ mlfromscratch/linear_red.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+
+class linearRegression:
+    def __init__(self, lr=0.001,epoch=1000):
+        self.lr = lr
+        self.epoch = epoch
+    
+    def fit(self, X,y):
+        pass
+    def predict(self, X):
+        pass
+    def loss(self):
+        pass
+    def gradient(self):
+        pass
+
+
+
+
+if __name__ == "__main__" :
+    pass
