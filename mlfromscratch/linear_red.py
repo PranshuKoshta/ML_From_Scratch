4f2c50f8f18e130195ca43037a2217c0f70dc891
Pranshu Koshta <pranshukoshta7@gmail.com>
2025-07-13 02:48:38 +0530

    lin reg & gradient descent

--- mlfromscratch/linear_red.py
+++ mlfromscratch/linear_red.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn import datasets
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
 class linearRegression:
@@ -10,16 +11,46 @@ class linearRegression:
         self.epoch = epoch
     
     def fit(self, X,y):
-        pass
+        n_samples, n_feature = X.shape
+
+        self.bias = 0
+        self.weight = np.zeros(n_feature)
+
+
+        for _ in range(self.epoch):
+            y_pred = np.dot(X,self.weight) + self.bias
+
+            dw = (1/n_samples)*np.dot(X.T,(y_pred - y))
+            db = (1/n_samples)*np.sum(y_pred - y)
+
+            self.weight -= self.lr*dw
+            self.bias  -= self.lr*db
+
     def predict(self, X):
-        pass
-    def loss(self):
-        pass
-    def gradient(self):
-        pass
+        y_predicted = np.dot(X,self.weight) + self.bias
+        return y_predicted
 
 
 
 
 if __name__ == "__main__" :
-    pass
+   
+    x,y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=123)
+    x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2, random_state=123)
+
+    lin_reg = linearRegression(lr=0.01)
+    lin_reg.fit(x_train,y_train)
+
+    predicted = lin_reg.predict(x_test)
+
+    mse = np.mean(np.sum(predicted - y_test)**2)
+
+    print(mse)
+
+    y_pred_line = lin_reg.predict(x)
+    cmap = plt.get_cmap("viridis")
+    fig = plt.figure(figsize=(8, 6))
+    m1 = plt.scatter(x_train, y_train, color=cmap(0.9), s=10)
+    m2 = plt.scatter(x_test, y_test, color=cmap(0.5), s=10)
+    plt.plot(x, y_pred_line, color="black", linewidth=2, label="Prediction")
+    plt.show()
